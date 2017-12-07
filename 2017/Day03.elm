9e185f63ea3b6d3287a3b8396a2aaa9b2e1c2110
Pierre Beitz <grimsen205@googlemail.com>
2017-12-07 11:01:48 +0100

    admit that 03/2 is not done yet :)

--- 2017/Day03.elm
+++ 2017/Day03.elm
@@ -6,6 +6,7 @@ import Html exposing (Html, text)
 main : Html msg
 main =
     { part1 = distanceOf 361527
+    , part2 = "TODO"
     }
         |> toString
         |> text
@@ -36,9 +37,9 @@ distanceOf n =
             [ center - 3 * r, center - r, center + r, center + 3 * r ]
     in
     centers
-        |> List.map (\c -> abs (n - c))
+        |> List.map (\center -> abs (n - center))
         |> List.minimum
-        |> Maybe.map ((+) r)
+        |> Maybe.map (\min -> min + r)
 
 
 
@@ -68,6 +69,16 @@ distanceOf n =
     5   4   3   2   3   4   5
     6   5   4   3   4   5   6
 
-   - middle number of each ring is 2n^2 + 1.
-   idea: find ring of number, compute diffs to smallest distances and take the smallest one. add this one to the ring-index.
+   - the average number of each ring is 2n^2 + 1.
+   idea: find ring of number, compute diffs to numbers on the cross through 1 and take the smallest one. add this one to the ring-index.
+   this means that we first "approach" to the cross through 1 and then move towards it.
+   here's a drawing containing multiple n's and their paths (to get the idea). (n in [36, 44, 49])
+
+   37  36--->---+  33  32  31
+   38  17  16   v  14  13  30
+   39  18   5   v   3  12  29
+   40  19   6   O   2  11  28
+   41  20   7   ^   9  10  27
+   42  21  22   ^  24  25  26
+   43  44--->---+---<---<--49
 -}
