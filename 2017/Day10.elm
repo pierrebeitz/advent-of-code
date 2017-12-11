f3ca55fcd81feb3df101031baec55fbaf41ab0fb
Pierre Beitz <grimsen205@googlemail.com>
2017-12-11 21:22:27 +0100

    Day 10

---
+++ 2017/Day10.elm
@@ -0,0 +1,88 @@
+module Main exposing (..)
+
+import Bitwise
+import Char
+import Hex
+import Html exposing (Html, text)
+import List exposing (product, range, reverse, take)
+import List.Extra as List exposing (groupsOf, splitAt)
+import String
+
+
+type alias State =
+    { stepSize : Int
+    , currentPosition : Int
+    , lengths : List Int
+    , list : List Int
+    }
+
+
+main : Html msg
+main =
+    { part1 = process part1InitialState |> .list |> take 2 |> product
+    , part2 = process64Rounds part2Input |> .list |> groupsOf 16 |> List.map (List.foldl1 Bitwise.xor >> Maybe.withDefault 0 >> Hex.toString) |> String.join ""
+    }
+        |> toString
+        |> text
+
+
+part1InitialState =
+    State 0 0 [ 70, 66, 255, 2, 48, 0, 54, 48, 80, 141, 244, 254, 160, 108, 1, 41 ] (range 0 255)
+
+
+{-| Idea:
+
+       1) 2 3 ([4] 5 -- Start
+    -> ([4] 5 1) 2 3 -- Move currentPosition to the front of the list.
+    -> ([1] 5 4) 2 3 -- Reverse n items. Here 3:
+    -> 4) 2 3 ([1] 5 -- Revert the first Operation.
+
+-}
+process ({ currentPosition, stepSize, lengths, list } as state) =
+    let
+        apply length =
+            list
+                |> moveToFront currentPosition
+                |> (splitAt length >> (\( fst, snd ) -> reverse fst ++ snd))
+                |> moveToFront (List.length list - currentPosition)
+    in
+    case lengths of
+        length :: restLengths ->
+            let
+                newPosition =
+                    (currentPosition + length + stepSize) % List.length list
+            in
+            process <| State (stepSize + 1) newPosition restLengths (apply length)
+
+        [] ->
+            state
+
+
+moveToFront i =
+    splitAt i >> (\( fst, snd ) -> snd ++ fst)
+
+
+
+-- Part2
+
+
+part2Input =
+    let
+        list =
+            "70,66,255,2,48,0,54,48,80,141,244,254,160,108,1,41"
+                |> String.toList
+                |> List.map Char.toCode
+                |> (\l -> l ++ [ 17, 31, 73, 47, 23 ])
+    in
+    State 0 0 list (range 0 255)
+
+
+process64Rounds initialState =
+    let
+        processHelp r state =
+            if r > 0 then
+                processHelp (r - 1) (process { state | lengths = initialState.lengths })
+            else
+                state
+    in
+    processHelp 64 initialState
